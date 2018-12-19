99566fd8f679234254232c510a54097f19753d9e
hedshefer <hed@leylinetech.com>
2018-12-19 13:40:44 +0200

    שינוי של קומנד אקואל

--- DefineVarCommand.cpp
+++ DefineVarCommand.cpp
@@ -4,6 +4,12 @@
 
 #include "DefineVarCommand.h"
 
-int DefineVarCommand::execute(vector<string> &str, int index) {
-    return 0;
+int DefineVarCommand::execute(vector<string> &str, int index,symbolTableNamePath, symbolTablePathDouble,
+        symbolTableNameDouble) {
+    if (str[index+3]=="bind"){
+        symbolTableNamePath.insert(str[index+1],str[index+4]);
+    } else{
+        new Expression
+        symbolTableNameDouble.insert(str[index+1],str[index+3]);
+    }
 }
---
+++ EqualCommand.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 12/19/2018.
+//
+
+#ifndef FIRSTSTONE_EQUALCOMMAND_H
+#define FIRSTSTONE_EQUALCOMMAND_H
+
+
+class EqualCommand {
+
+};
+
+
+#endif //FIRSTSTONE_EQUALCOMMAND_H
